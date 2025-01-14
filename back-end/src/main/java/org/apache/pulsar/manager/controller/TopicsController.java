9453c1f042658657d36245f498d177886750bf4f
goncaloribeiro <goncalocribeiro@tecnico.ulisboa.pt>
2025-01-14 00:23:51 +0000

    fix: move TopicsController from javax to jakarta

--- back-end/src/main/java/org/apache/pulsar/manager/controller/TopicsController.java
+++ back-end/src/main/java/org/apache/pulsar/manager/controller/TopicsController.java
@@ -32,9 +32,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.Size;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
 import java.util.List;
 import java.util.Map;
 

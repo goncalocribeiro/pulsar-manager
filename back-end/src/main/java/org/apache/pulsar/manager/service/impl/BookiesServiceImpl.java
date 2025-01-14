fd872a93d06a1af73b0a55c781dc49345032e603
goncaloribeiro <goncalocribeiro@tecnico.ulisboa.pt>
2025-01-14 00:22:00 +0000

    fix: move BookiesServiceImpl from javax to jakarta

--- back-end/src/main/java/org/apache/pulsar/manager/service/impl/BookiesServiceImpl.java
+++ back-end/src/main/java/org/apache/pulsar/manager/service/impl/BookiesServiceImpl.java
@@ -25,7 +25,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.HashMap;
